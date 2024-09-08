5732f67d0c12e91dc93662f6dd9d20c6eb26dec6
ApuravDivekar2032 <126816224+ApuravDivekar2032@users.noreply.github.com>
2024-09-08 19:42:24 +0530

    Update server.py

--- Server/server.py
+++ Server/server.py
@@ -1,7 +1,12 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_from_directory
+import os
 import util
 
 app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Welcome to the Real Estate Price Prediction API"
 
 @app.route('/get_location_names', methods=['GET'])
 def get_location_names():
@@ -9,7 +14,6 @@ def get_location_names():
         'locations': util.get_location_names()
     })
     response.headers.add('Access-Control-Allow-Origin', '*')
-
     return response
 
 @app.route('/predict_home_price', methods=['GET', 'POST'])
@@ -20,13 +24,16 @@ def predict_home_price():
     bath = int(request.form['bath'])
 
     response = jsonify({
-        'estimated_price': util.get_estimated_price(location,total_sqft,bhk,bath)
+        'estimated_price': util.get_estimated_price(location, total_sqft, bhk, bath)
     })
     response.headers.add('Access-Control-Allow-Origin', '*')
-
     return response
+
+@app.route('/client/<path:app.html>')
+def serve_client_static():
+    return send_from_directory('client', app.html)
 
 if __name__ == "__main__":
     print("Starting Python Flask Server For Home Price Prediction...")
     util.load_saved_artifacts()
-    app.run()
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
